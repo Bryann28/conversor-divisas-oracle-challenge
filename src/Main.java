d1023fd414d3c9b1d0c53c03c0aaf5208150edec
BryanHH <brayann110200@gmail.com>
2024-06-27 13:13:30 -0600

    En el main se crea el objeto consulta = new ConsultaMoneda(), se creaun switch para las opciones mostradas, cada case contiene un metodo, el 1 al 6 contiene comparativos de monedas para una rapida comparacion recibe una monedaBase y un monedaTarget predefinidas en el programa tambien consulta y lectura para poder convertir correctamente. El case 7 contiene el metodo convertirOtraMoneda para que el usuario pueda ingresar dos codigos de monedas a consultar, ingresa la cantidad y devuelve el tipo de cambio. En el case 8 muestra el mensaje, Saliendo..., y termina el programa. Cada case contiene un break para reiniciar el programa exeptuando el case 8 el cual lo termina. El switch contiene un default, para en caso de elegir una opcion diferente a las validas muestre el mensaje, Opcion no valida, y puedes reintentar sin reiniciar manualmente el programa.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner lectura = new Scanner(System.in);
+        ConsultaMoneda consulta = new ConsultaMoneda();
 
         int opcion = 0;
         while (opcion != 8) {
@@ -20,6 +21,35 @@ public class Main {
                     "•♦•♦•♦•♦•♦•♦•♦•♦•♦•♦•♦•♦•♦•♦•♦•♦•♦•♦•♦•♦•♦•♦•\n");
             opcion = lectura.nextInt();
             lectura.nextLine();
+
+            switch (opcion){
+                case 1:
+                    ConvertirMoneda.convertir("USD", "ARS", consulta, lectura);
+                    break;
+                case 2:
+                    ConvertirMoneda.convertir("ARS", "USD", consulta, lectura);
+                    break;
+                case 3:
+                    ConvertirMoneda.convertir("USD", "BRL", consulta, lectura);
+                    break;
+                case 4:
+                    ConvertirMoneda.convertir("BRL", "USD", consulta, lectura);
+                    break;
+                case 5:
+                    ConvertirMoneda.convertir("USD", "MXN", consulta, lectura);
+                    break;
+                case 6:
+                    ConvertirMoneda.convertir("MXN", "USD", consulta, lectura);
+                    break;
+                case 7:
+                    ConvertirMoneda.convertirOtraMoneda(consulta, lectura);
+                    break;
+                case 8:
+                    System.out.println("Saliendo...");
+                default:
+                    System.out.println("Opción no válida");
+                    break;
+            }
         }
     }
 }
